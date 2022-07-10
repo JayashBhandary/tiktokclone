3944a98278a6abae493397423d6dba5557ae3d85
Jayash Bhandary <jayashbhandary@Jayashs-iMac.local>
2022-07-10 18:59:28 +0530

    first new

---
+++ types.d.ts
@@ -0,0 +1,38 @@
+export interface Video {
+    caption: string;
+    video: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+    };
+    _id: string;
+    postedBy: {
+      _id: string;
+      name: string;
+      image: string;
+    };
+    likes: {
+      postedBy: {
+        _id: string;
+        userName: string;
+        image: string;
+      };
+    }[];
+    comments: {
+      comment: string;
+      _key: string;
+      postedBy: {
+        _ref: string;
+      };
+    }[];
+    userId: string;
+  }
+  
+  export interface IUser {
+    name: ReactNode;
+    _id: string;
+    _type: string;
+    name: string;
+    image: string;
+  }
